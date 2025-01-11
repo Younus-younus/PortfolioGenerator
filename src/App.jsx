e88a51f4522500d02fa051741b902a6f2725b903
Younus <you@example.com>
2025-01-11 10:23:16 +0530

    feat: Add responsive design to portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from './components/NavBar'
 import HomePage from './components/HomePage'
 import AuthorPortfolio from './components/AuthorPortfolio'
