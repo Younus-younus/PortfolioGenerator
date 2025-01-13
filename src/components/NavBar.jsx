047746eabb1e54d2044ddf603861154e1a1bf3fd
Younus <you@example.com>
2025-01-13 15:03:40 +0530

    edited

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,7 +17,7 @@ export default function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg sticky-top nav-underline">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/api/resumes/portfolios">
+                <Link className="navbar-brand" to="/">
                     <b>Resume</b>
                 </Link>
                 <button
